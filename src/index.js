d24a023a36f2fc5fd014e26caf4c0017118ebc6d
claudieux <ciobanuclaudiumihai@gmail.com>
2024-06-25 18:14:06 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Create this file for basic styling
+import Dashboard from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
